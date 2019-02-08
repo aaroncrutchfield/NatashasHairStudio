49b7dd8fcfb3d3f512426cb5855a01fa46782366
Aaron Crutchfield <aaroncrutchfield09@gmail.com>
2019-02-07 20:53:23 -0500

    feat: Add methods for signing in and out

--- app/src/main/java/com/acrutchfield/natashashairstudio/ui/MainActivity.java
+++ app/src/main/java/com/acrutchfield/natashashairstudio/ui/MainActivity.java
@@ -1,17 +1,33 @@
 package com.acrutchfield.natashashairstudio.ui;
 
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.acrutchfield.natashashairstudio.R;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Arrays;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // TODO: 2/7/19 Check if the user is signed in
+    // TODO: 2/7/19 Prompt the user to sign in if they aren't already
+    // TODO: 2/7/19 Find a way to show the logo by default if the user isn't signed in
+    private static final int REQUEST_SIGN_IN = 0;
+    public static final String SIGNED_OUT = "Signed Out";
     private TextView mTextMessage;
+    private FragmentManager fragmentManager;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -41,6 +57,36 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        fragmentManager = getSupportFragmentManager();
+    }
+
+    // TODO: 2/7/19 Setup signing in with Email
+    private void signIn() {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setLogo(R.drawable.logo)
+                        .setTheme(R.style.AppTheme)
+                        .build(),
+                REQUEST_SIGN_IN
+        );
+    }
+
+    private void signOut() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(MainActivity.this, SIGNED_OUT, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
 }
--- app/src/main/java/com/acrutchfield/natashashairstudio/ui/ReviewFragment.java
+++ app/src/main/java/com/acrutchfield/natashashairstudio/ui/ReviewFragment.java
@@ -1,16 +1,17 @@
 package com.acrutchfield.natashashairstudio.ui;
 
-import androidx.lifecycle.ViewModelProviders;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.acrutchfield.natashashairstudio.R;
 import com.acrutchfield.natashashairstudio.viewmodel.ReviewViewModel;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 public class ReviewFragment extends Fragment {
 
