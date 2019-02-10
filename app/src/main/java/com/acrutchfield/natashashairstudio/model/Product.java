4bb4e065a8b65a93d3589f6a0339874452036d53
Aaron Crutchfield <aaroncrutchfield09@gmail.com>
2019-02-10 08:37:26 -0500

    feat: Create models for Product and ProductCollection

---
+++ app/src/main/java/com/acrutchfield/natashashairstudio/model/Product.java
@@ -0,0 +1,11 @@
+package com.acrutchfield.natashashairstudio.model;
+
+import android.graphics.Bitmap;
+
+import java.util.Map;
+
+public class Product {
+    private Bitmap image;
+    private Map<Integer, Long> length;
+    private String priceRange;
+}
---
+++ app/src/main/java/com/acrutchfield/natashashairstudio/model/ProductCollection.java
@@ -0,0 +1,10 @@
+package com.acrutchfield.natashashairstudio.model;
+
+import android.graphics.Bitmap;
+
+import java.util.List;
+
+public class ProductCollection {
+    private List<Product> productList;
+    private Bitmap image;
+}
