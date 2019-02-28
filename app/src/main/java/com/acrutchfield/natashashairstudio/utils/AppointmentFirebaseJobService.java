e2f5881d52372e9f7bc163822e4d6c2cbb62d89b
Aaron Crutchfield <aaroncrutchfield09@gmail.com>
2019-02-28 08:46:44 -0500

    fix: Cancel recurring reminder notification when switch is off

--- app/src/main/java/com/acrutchfield/natashashairstudio/utils/AppointmentFirebaseJobService.java
+++ app/src/main/java/com/acrutchfield/natashashairstudio/utils/AppointmentFirebaseJobService.java
@@ -1,7 +1,5 @@
 package com.acrutchfield.natashashairstudio.utils;
 
-import android.os.AsyncTask;
-
 import com.firebase.jobdispatcher.JobParameters;
 import com.firebase.jobdispatcher.JobService;
 
@@ -9,25 +7,28 @@ import androidx.annotation.NonNull;
 
 public class AppointmentFirebaseJobService extends JobService {
 
-    AsyncTask mBackgroundTask;
 
     @Override
     public boolean onStartJob(@NonNull JobParameters job) {
-        AppExecutors.getInstance().diskIO().execute(() -> {
-            AppointmentReminderTask.execute(
-                    AppointmentFirebaseJobService.this,
-                    AppointmentReminderTask.ACTION_APPOINTMENT_REMINDER
-            );
 
-            jobFinished(job, false);
-        });
+        if (SharedPrefs.getReminder(getBaseContext())) {
+            AppExecutors.getInstance().diskIO().execute(() -> {
+                AppointmentReminderTask.execute(
+                        AppointmentFirebaseJobService.this,
+                        AppointmentReminderTask.ACTION_APPOINTMENT_REMINDER
+                );
 
-        return true;
+                jobFinished(job, false);
+            });
+            return true;
+        } else {
+            AppointmentReminderUtils.cancelAppointmentReminder(getBaseContext());
+            return false;
+        }
     }
 
     @Override
     public boolean onStopJob(@NonNull JobParameters job) {
-//        if (mBackgroundTask != null) mBackgroundTask.cancel(true);
         return false;
     }
 
